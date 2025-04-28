776998677ff64e32546a6b4b65bf2d97716276ee
avinash-9-9 <99avinashverma@gmail.com>
2025-04-28 13:07:15 +0530

    updated data

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,24 +21,13 @@ export const aboutData = {
   paragraphOne:
     'I am a dedicated web developer who specializes in developing full-stack web applications using practical frameworks.I am passionate about solving problems—a good solution usually requires a dose of creativity that then requires more effort to adapt and perfect.',
   paragraphTwo:
-    'I speacialize in Reactjs | NodeJs | ExpressJs | MongoDB | Redux | CSS | Bootstrap | HTML | Javascript | Firebase | Python | Flask | SASS | Socket.IO | Multer | React Hooks |',
+    'I speacialize in Angular | Reactjs | NodeJs | ExpressJs | MongoDB | Redux | CSS | HTML | Javascript | Typescript | SASS | React Hooks |',
   paragraphThree: 'I love to code and create.',
   resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
-  {
-    id: nanoid(),
-    img: 'fullecom.png',
-    title: 'Ecommerce Web App',
-    info:
-      'Ecommerce website built with MERN stack, React Context API for state management, pure CSS for style with paypal checkout integration.',
-    info2:
-      'Created backend express server. Mongodb to store the data in backend. React functional components to design the user interface. React context api for state management. CSS for styling. Cloudinary for image storage.Heroku for deployment is used.',
-    url: 'https://avinash-react-fullecommerce.herokuapp.com/',
-    repo: 'https://github.com/avi-verma-web/fullEcommerce-Mern-react', // if no repo, the button will not show up
-  },
   {
     id: nanoid(),
     img: 'chatSock.PNG',
